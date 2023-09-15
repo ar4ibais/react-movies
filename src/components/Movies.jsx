5b4eb297dc2622cb531e46fdcabbe5a269770751
ar4ibais <ak.hutov@yandex.ru>
2023-09-15 13:27:39 +0300

    worked with cards and fetch data for films

---
+++ src/components/Movies.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Movie from "./Movie";
+
+function Movies(props) {
+    const { movies } = props;
+
+    return (
+        <div className="movies">
+            {movies.map(movie => (
+                <Movie key={movie.imdbID} {...movie} />
+            ))}
+        </div>
+    )
+}
+
+export default Movies;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,26 @@
-function Main() {
-    return (
-        <main className="content container">
-            Hello world!
-        </main>
-    )
+import React, { Component } from "react";
+
+import Movies from "../components/Movies";
+
+class Main extends Component {
+    state = {
+        movies: []
+    }
+    componentDidMount() {
+        fetch("http://www.omdbapi.com/?apikey=5dad9cc6&s=matrix")
+            .then(res => res.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+    render() {
+        const { movies } = this.state;
+        return (
+            <main className="content container">
+                {
+                    movies.length ? (<Movies movies={this.state.movies} />) : <h5>Loading...</h5>
+                }
+            </main>
+        )
+    }
 }
 
 export default Main;
